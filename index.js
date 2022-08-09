e019acce897e39adbf1f7709727276ade25d1527
Isaac Kim <kisaac95@yahoo.com>
2022-08-08 19:02:51 -0700

    v1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your Code Here
+let playerNm = window.prompt('Please enter your name: ')
+window.alert('Welcome to my trivia game '+playerNm)
+let playerScore = 0
+var playAgain = true
+
+while(playAgain === true){
+    playGame()
+    let playerChoice = window.prompt('Would you like to play again? Yes or No?')
+    if (playerChoice == 'yes' || playerChoice == 'Yes'){
+         playAgain = true
+    } else {
+         playAgain = true
+        window.alert('Thank you for playing with me!')
+    }
+}
+function playGame(){
+    for(let i=0; i < questions.length; i++){
+        let question = questions[i]
+        let playerAnswer = window.prompt(question.text)
+        if (playerAnswer == question.correctAnswer){
+            playerScore = playerScore + 10
+        }
+    }
+    window.alert('Your final score is: '+playerScore)
+}
+
